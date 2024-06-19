aef64f80fe4b667454492b1b36c1aca9bd5a99c9
Your Name <you@example.com>
2024-06-20 01:25:06 +0300

    register adapter

--- lib/cubits/add_note_cubit/add_note_cubit.dart
+++ lib/cubits/add_note_cubit/add_note_cubit.dart
@@ -1,8 +1,14 @@
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
+import 'package:notes_app/models/note_model.dart';
 
 part 'add_note_state.dart';
 
 class NoteCubit extends Cubit<AddNoteState> {
   NoteCubit(): super(AddNoteInitial());
+
+
+  addNote(NoteModel note){
+
+  }
 }
