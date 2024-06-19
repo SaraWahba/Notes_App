7a84ddd93dc3e659e0d6415498a9d7508fc90e08
Your Name <you@example.com>
2024-06-20 01:18:18 +0300

    add add note state

--- lib/cubits/add_note_cubit/add_note_cubit.dart
+++ lib/cubits/add_note_cubit/add_note_cubit.dart
@@ -3,6 +3,6 @@ import 'package:meta/meta.dart';
 
 part 'add_note_state.dart';
 
-class NotesCubit extends Cubit<NotesState> {
-  NotesCubit(): super(NotesInitial());
+class NoteCubit extends Cubit<AddNoteState> {
+  NoteCubit(): super(AddNoteInitial());
 }
