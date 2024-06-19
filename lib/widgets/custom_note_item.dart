45e23c5812fa7cd9df5ca76c6725dc2e8c298ea0
Your Name <you@example.com>
2024-06-19 19:27:42 +0300

    create Edit Note View

--- lib/widgets/custom_note_item.dart
+++ lib/widgets/custom_note_item.dart
@@ -1,57 +1,65 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:notes_app/views/edit_note_view.dart';
 class NoteItem extends StatelessWidget {
   const NoteItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
-      decoration: BoxDecoration(
-        color: Color(0xffFFCC80),
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            title: const Text("Flutter Tips",
+    return GestureDetector(
+      onTap: (){
+        Navigator.push(context, MaterialPageRoute(builder: (context){
+          return const EditNoteView();
+        }),);
+      },
+      child: Container(
+        padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
+        decoration: BoxDecoration(
+          color: Color(0xffFFCC80),
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              title: const Text("Flutter Tips",
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  )),
+              subtitle: Padding(
+                padding: const EdgeInsets.only(top: 17, bottom: 16),
+                child: Text("Build your career with Sara Wahba",
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.black.withOpacity(.6),
+                  ),
+                ),
+              ),
+              trailing: IconButton(
+                  icon: const Padding(
+                    padding: EdgeInsets.only(left: 11),
+                    child: Icon(
+                      FontAwesomeIcons.trash,
+                      color: Colors.black,
+                      size: 24,
+                    ),
+                  ),
+                  onPressed: () {}),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(right: 24),
+              child: Text(
+                "May21,2024",
                 style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black,
-                )),
-            subtitle: Padding(
-              padding: const EdgeInsets.only(top: 17, bottom: 16),
-              child: Text("Build your career with Sara Wahba",
-                style: TextStyle(
-                  fontSize: 18,
+                  fontSize: 16,
                   color: Colors.black.withOpacity(.6),
                 ),
               ),
-            ),
-            trailing: IconButton(
-                icon: const Padding(
-                  padding: EdgeInsets.only(left: 11),
-                  child: Icon(
-                    FontAwesomeIcons.trash,
-                    color: Colors.black,
-                    size: 24,
-                  ),
-                ),
-                onPressed: () {}),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(right: 24),
-            child: Text(
-              "May21,2024",
-              style: TextStyle(
-                fontSize: 16,
-                color: Colors.black.withOpacity(.6),
-              ),
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
