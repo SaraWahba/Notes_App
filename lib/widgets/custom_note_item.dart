7708260ccdd221ffe51340d1923cff8c0ea5ea9f
Your Name <you@example.com>
2024-06-19 12:55:38 +0300

    create Custom Note Item

---
+++ lib/widgets/custom_note_item.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+class NoteItem extends StatelessWidget {
+  const NoteItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
+      decoration: BoxDecoration(
+        color: Color(0xffFFCC80),
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          ListTile(
+            title: Text("Flutter Tips",
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                )),
+            subtitle: Padding(
+              padding: const EdgeInsets.only(top: 17, bottom: 16),
+              child: Text("Build your career with Sara Wahba",
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Colors.black.withOpacity(.5),
+                ),
+              ),
+            ),
+            trailing: IconButton(
+                icon: Padding(
+                  padding: const EdgeInsets.only(left: 11),
+                  child: Icon(
+                    FontAwesomeIcons.trash,
+                    color: Colors.black,
+                    size: 24,
+                  ),
+                ),
+                onPressed: () {}),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 24),
+            child: Text(
+              "May21,2024",
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.black.withOpacity(.5),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
