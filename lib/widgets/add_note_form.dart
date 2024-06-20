4e9e9a0fe30e7501d825af9f743d9124f78cf28e
Your Name <you@example.com>
2024-06-20 13:30:21 +0300

    Trigger add note cubit

--- lib/widgets/add_note_form.dart
+++ lib/widgets/add_note_form.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_app/cubits/add_note_cubit/add_note_cubit.dart';
+import 'package:notes_app/models/note_model.dart';
 import 'package:notes_app/widgets/custom_botton.dart';
 import 'package:notes_app/widgets/custom_text_field.dart';
+
 class AddNoteForm extends StatefulWidget {
   const AddNoteForm({super.key});
 
@@ -38,16 +42,23 @@ class _AddNoteFormState extends State<AddNoteForm> {
           SizedBox(
             height: 45,
           ),
-          CustomButton(onTap: (){
-            if(formKey.currentState!.validate()){
-              formKey.currentState!.save();
-            }else{
-              autovalidateMode = AutovalidateMode.always;
-              setState(() {
+          CustomButton(
+            onTap: () {
+              if (formKey.currentState!.validate()) {
+                formKey.currentState!.save();
 
-              });
-            }
-          },),
+                var noteModel = NoteModel(
+                    title: title!,
+                    subTitle: subTitle!,
+                    date: DateTime.now().toString(),
+                    color: Colors.cyan.value);
+                BlocProvider.of<AddNoteCubit>(context).addNote(noteModel);
+              } else {
+                autovalidateMode = AutovalidateMode.always;
+                setState(() {});
+              }
+            },
+          ),
           SizedBox(
             height: 20,
           ),
