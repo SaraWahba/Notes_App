aa8a1b609205006dd9e5522b18dd4b5ad2ee48e6
Your Name <you@example.com>
2024-06-21 14:26:23 +0300

    create fetch note method

--- lib/cubits/notes_cubit/notes_cubit.dart
+++ lib/cubits/notes_cubit/notes_cubit.dart
@@ -1,5 +1,7 @@
 import 'package:bloc/bloc.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:notes_app/constants.dart';
 import 'package:notes_app/models/note_model.dart';
 
 
@@ -7,4 +9,15 @@ part 'notes_state.dart';
 
 class NotesCubit extends Cubit<NotesState> {
   NotesCubit() : super (NotesInitial());
+
+
+  fetchAllNotes() async {
+    try {
+      var notesBox = Hive.box<NoteModel>(kNoteBox);
+      List<NoteModel> notes  = notesBox.values.toList();
+      emit(NotesSuccess(notes));
+    }catch (e) {
+      emit(NotesFailure(e.toString()));
+    }
+  }
 }
--- lib/cubits/notes_cubit/notes_state.dart
+++ lib/cubits/notes_cubit/notes_state.dart
@@ -5,15 +5,15 @@ abstract class NotesState {}
 
 class NotesInitial extends NotesState {}
 
-class AddNoteLoading extends NotesState {}
+class NotesLoading extends NotesState {}
 
-class AddNoteSuccess extends NotesState {
+class NotesSuccess extends NotesState {
   List<NoteModel> notes;
-  AddNoteSuccess(this.notes);
+  NotesSuccess(this.notes);
 }
 
-class AddNoteFailure extends NotesState {
+class NotesFailure extends NotesState {
   final String errMessage;
 
-  AddNoteFailure(this.errMessage);
+  NotesFailure(this.errMessage);
 }
