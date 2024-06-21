a42e5987b4b451d5d5faf3425fc34cff3e1638bf
Your Name <you@example.com>
2024-06-21 19:30:00 +0300

    edit note done

--- lib/widgets/custom_app_bar.dart
+++ lib/widgets/custom_app_bar.dart
@@ -1,21 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/widgets/custom_icon.dart';
+
 class CustomAppBar extends StatelessWidget {
-  const CustomAppBar({super.key, required this.title, required this.icon});
+  const CustomAppBar(
+      {super.key, required this.title, required this.icon, this.onPressed});
   final String title;
-  final IconData icon ;
+  final IconData icon;
+  final void Function()? onPressed;
   @override
   Widget build(BuildContext context) {
-    return  Row(
+    return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
-        Text(title,
-           style: TextStyle(
-             fontSize: 28
-           ),
+        Text(
+          title,
+          style: TextStyle(fontSize: 28),
         ),
         Spacer(),
-        CustomIcon(icon: icon),
+        CustomIcon(onPressed: onPressed, icon: icon),
       ],
     );
   }
