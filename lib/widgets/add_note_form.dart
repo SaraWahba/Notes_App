0d75d51f1403471c6d6c9226a29273e9ccb3f2c1
Your Name <you@example.com>
2024-06-22 12:20:20 +0300

    create colors list view part one

--- lib/widgets/add_note_form.dart
+++ lib/widgets/add_note_form.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:intl/intl.dart';
 import 'package:notes_app/cubits/add_note_cubit/add_note_cubit.dart';
 import 'package:notes_app/models/note_model.dart';
+import 'package:notes_app/widgets/colors_list_view.dart';
 import 'package:notes_app/widgets/custom_botton.dart';
 import 'package:notes_app/widgets/custom_text_field.dart';
 
@@ -41,8 +42,13 @@ class _AddNoteFormState extends State<AddNoteForm> {
               hint: 'Content',
               maxLines: 5),
           SizedBox(
-            height: 45,
+            height: 25,
           ),
+          ColorsListView(),
+          SizedBox(
+            height: 25,
+          ),
+
           BlocBuilder<AddNoteCubit, AddNoteState>(builder: (context, state) {
             return CustomButton(
               isLoading: state is AddNoteLoading ? true : false,
@@ -73,3 +79,8 @@ class _AddNoteFormState extends State<AddNoteForm> {
     );
   }
 }
+
+
+
+
+
